3871b485515f83b85c4451af436f3db4e51e344a
Roman Sheport <example@gmail.com>
2022-02-19 19:01:50 +0200

    firts commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*jslint node: true */
+'use strict';
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user ='Ivan';
+alert(`Привет, ${user}`);
+
+let poter = 10,
+    decr = 10;
+
+poter++;
+decr--;
+
+console.log(poter);
+console.log(decr);
